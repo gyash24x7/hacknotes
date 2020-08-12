e1b3e8ac2d052f20f305af4a03c148d802151dc7
gyash24x7 <gyash24x7@gmail.com>
2020-08-12 16:28:07 +0530

    update dependencies

--- web/src/components/AppCard.tsx
+++ web/src/components/AppCard.tsx
@@ -2,7 +2,7 @@ import { colors } from "@atlaskit/theme";
 import styled from "styled-components";
 
 export const AppCard = styled.div`
-	width: calc(100% - 52px);
+	width: calc(100% - 42px);
 	border: 1px solid ${colors.N50};
 	padding: 20px;
 	border-radius: 5px;
