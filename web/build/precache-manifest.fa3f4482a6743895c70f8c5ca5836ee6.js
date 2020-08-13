47ffd99efd04696495e16728163ba35199f0d24a
gyash24x7 <gyash24x7@gmail.com>
2020-08-13 21:24:00 +0530

    added web build to git

---
+++ web/build/precache-manifest.fa3f4482a6743895c70f8c5ca5836ee6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcd6886399031457f6296b5c7d0bb15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d70c5e870d41e770e441",
+    "url": "/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "d70c5e870d41e770e441",
+    "url": "/static/js/2.39cfaf31.chunk.js"
+  },
+  {
+    "revision": "56ff208faacf53e93daa0a9b20f9acf1",
+    "url": "/static/js/2.39cfaf31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4fae1148ece1717c37",
+    "url": "/static/js/main.b3c93355.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "ff17e284a901dd67fe6b8629e9ffc43b",
+    "url": "/static/media/absbg.ff17e284.png"
+  },
+  {
+    "revision": "07376b7e59446cd5d8ba786f36a1e6c6",
+    "url": "/static/media/icon.07376b7e.svg"
+  },
+  {
+    "revision": "aa43db230831b02fc806f34b24374d84",
+    "url": "/static/media/integrated-logo.aa43db23.svg"
+  },
+  {
+    "revision": "f69685ed9bc1e58d1c08684d13996867",
+    "url": "/static/media/wordmark.f69685ed.svg"
+  }
+]);
